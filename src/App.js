a71e8b3439e262dcaedf9d6868f1ad706a78d91a
kael <dev.paycial@gmail.com>
2019-06-10 02:33:59 +0900

    1.금일 강좌들은거

--- src/App.js
+++ src/App.js
@@ -19,28 +19,28 @@ const moviesImages = [
   "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
 ]
 
-const movies = [
-  {
-    id: 1,
-    title: "Matrix",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-  },
-  {
-    id: 2,
-    title: "Full Metal Jacket",
-    poster: "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2017/05/full_metal_jacket_1987_3-h_2017.jpg"
-  },
-  {
-    id: 3,
-    title: "Oldboy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-  },
-  {
-    id: 4,
-    title: "Star Wars",
-    poster: "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
-  }
-]
+// const movies = [
+//   {
+//     id: 1,
+//     title: "Matrix",
+//     poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+//   },
+//   {
+//     id: 2,
+//     title: "Full Metal Jacket",
+//     poster: "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2017/05/full_metal_jacket_1987_3-h_2017.jpg"
+//   },
+//   {
+//     id: 3,
+//     title: "Oldboy",
+//     poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+//   },
+//   {
+//     id: 4,
+//     title: "Star Wars",
+//     poster: "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+//   }
+// ]
 
 
 
@@ -73,31 +73,42 @@ class App extends Component {
 
   // Delete : componentWillUnmount
 
+  state = {
+
+  }
+
+
+
   componentWillMount() {
     console.log("will mount");
   }
 
 
   componentDidMount() {
-    console.log("did mount");
+
+    fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+        // .then(response => console.log(response))
+        .then(potato => potato.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err))
+
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie,index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })  // [<Movie props />,<Movie props />] 같은 표현
+
+
+
+    return movies
   }
 
   render() {
     console.log("did render");
     return (
         <div className="App">
-          {/*<Movie title={moviesTitles[0]} poster={moviesImages[0]}/>*/}
-          {/*<Movie title={moviesTitles[1]} poster={moviesImages[1]}/>*/}
-          {/*<Movie title={moviesTitles[2]} poster={moviesImages[2]}/>*/}
-          {/*<Movie title={moviesTitles[3]} poster={moviesImages[3]}/>*/}
-          {/*{movies.map(movie => {*/}
-          {/*return <Movie title={movie.title} poster={movie.poster}/>*/}
-          {/*})}*/}
-
-          {movies.map((movie, index) => {
-            return <Movie title={movie.title} poster={movie.poster} key={index}/>
-          })}
-
+            {this.state.movies ? this._renderMovies() : 'Loading'}
         </div>
     );
 
--- src/Movie.js
+++ src/Movie.js
@@ -2,37 +2,63 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
+// class Movie extends Component{
+//
+//   static propTypes = {
+//     title: PropTypes.string.isRequired,
+//     poster: PropTypes.string.isRequired
+//   }
+//
+//   render() {
+//     console.log(this.props);
+//     return (
+//         <div>
+//           <MoviePoster poster={this.props.poster}/>
+//           <h1>{this.props.title}</h1>
+//         </div>
+//         )
+//   }
+// }
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-  }
+// class MoviePoster extends Component{
+//
+//   static propTypes = {
+//     poster: PropTypes.string.isRequired
+//   }
+//
+//   render(){
+//     console.log(this.props.poster);
+//     return(
+//         <img src={this.props.poster}/>
+//     )
+//   }
+//
+// }
 
-  render() {
-    console.log(this.props);
+function Movie({title, poster}) {
+
     return (
         <div>
-          <MoviePoster poster={this.props.poster}/>
-          <h1>{this.props.title}</h1>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
         </div>
-        )
-  }
+    )
 }
 
-class MoviePoster extends Component{
 
-  static propTypes = {
-    poster: PropTypes.string.isRequired
-  }
-
-  render(){
-    console.log(this.props.poster);
-    return(
-        <img src={this.props.poster}/>
+function MoviePoster({poster}) {
+    return (
+        <img src={poster} alt="Movie Poster"/>
     )
-  }
+}
 
+Movie.prototype = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+MoviePoster.prototype = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
