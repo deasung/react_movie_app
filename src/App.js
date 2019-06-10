8f65d278687571c994a9b411cb9453838a908f96
kael <dev.paycial@gmail.com>
2019-06-11 00:50:57 +0900

    1.금일까지 들은거 커밋

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 import Movie from './Movie';
 
 const moviesTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
+    "Matrix",
+    "Full Metal Jacket",
+    "Oldboy",
+    "Star Wars"
 ]
 //https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg
 //https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2017/05/full_metal_jacket_1987_3-h_2017.jpg
 //https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg
 //https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg
 const moviesImages = [
-  "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
-  "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2017/05/full_metal_jacket_1987_3-h_2017.jpg",
-  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-  "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+    "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
+    "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2017/05/full_metal_jacket_1987_3-h_2017.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+    "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
 ]
 
 // const movies = [
@@ -43,7 +43,6 @@ const moviesImages = [
 // ]
 
 
-
 // function App() {
 //   return (
 //       <div className="App">
@@ -67,53 +66,71 @@ const moviesImages = [
 class App extends Component {
 
 
-  // Render : componentWillMount -> render -> componentDidMount
+    // Render : componentWillMount -> render -> componentDidMount
 
-  // Update : componentWillReceiveProps -> shouldComponentUpdate -> componentWillUpdate-> render -> componentDidUpdate
+    // Update : componentWillReceiveProps -> shouldComponentUpdate -> componentWillUpdate-> render -> componentDidUpdate
 
-  // Delete : componentWillUnmount
+    // Delete : componentWillUnmount
 
-  state = {
-
-  }
+    state = {}
 
 
+    componentWillMount() {
+        console.log("will mount");
+    }
 
-  componentWillMount() {
-    console.log("will mount");
-  }
+
+    componentDidMount() {
+        this._getMovies();
+    }
+
+    _renderMovies = () => {
+        const movies = this.state.movies.map((movie) => {
+
+            console.log(movie);
+
+            return <Movie title={movie.title}
+                          poster={movie.medium_cover_image}
+                          key={movie.id}
+                          genres={movie.genres}
+                          synopsis={movie.synopsis}
+                    />
+        })  // [<Movie props />,<Movie props />] 같은 표현
 
 
-  componentDidMount() {
+        return movies
+    }
 
-    fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+    _getMovies = async () => {
+
+        const movies = await this._callApi()
+        this.setState({
+            movies
+        })
+
+    }
+
+    _callApi = () => {
+        return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=download_count')
         // .then(response => console.log(response))
-        .then(potato => potato.json())
-        .then(json => console.log(json))
-        .catch(err => console.log(err))
+            .then(potato => potato.json())
+            .then(json => json.data.movies)
+            .catch(err => console.log(err))
+    }
 
-  }
+    render() {
+        console.log("did render");
 
-  _renderMovies = () => {
-    const movies = this.state.movies.map((movie,index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index}/>
-    })  // [<Movie props />,<Movie props />] 같은 표현
+        const { movies } = this.state;
 
-
-
-    return movies
-  }
-
-  render() {
-    console.log("did render");
-    return (
-        <div className="App">
-            {this.state.movies ? this._renderMovies() : 'Loading'}
-        </div>
-    );
+        return (
+            <div className={movies ? "App" : "App--loading"}>
+                {this.state.movies ? this._renderMovies() : 'Loading'}
+            </div>
+        );
 
 
-  }
+    }
 
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
-
+import LinesEllipsis from 'react-lines-ellipsis';
 // class Movie extends Component{
 //
 //   static propTypes = {
@@ -35,30 +35,66 @@ import './Movie.css';
 //
 // }
 
-function Movie({title, poster}) {
+function Movie({title, poster, genres, synopsis}) {
 
     return (
-        <div>
-            <MoviePoster poster={poster}/>
-            <h1>{title}</h1>
+        <div className="Movie">
+
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster} alt={title}/>
+            </div>
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre,index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <div className="Movie__Synopsis">
+                    <LinesEllipsis
+                        text={synopsis}
+                        maxLine='3'
+                        ellipsis='...'
+                        trimRight
+                        basedOn='letters'
+                    />
+
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
 
 
-function MoviePoster({poster}) {
+function MoviePoster({poster,alt}) {
     return (
-        <img src={poster} alt="Movie Poster"/>
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster"/>
     )
 }
 
+
+function MovieGenre({genre}) {
+    return (
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
+
 Movie.prototype = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.prototype = {
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenre.prototype = {
+    genres: PropTypes.string.isRequired
 }
 
 export default Movie;
